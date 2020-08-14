3657a8f110abdf9f17e644882243953d56f576b3
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2020-08-14 15:31:49 +0200

    read method

--- database/products.inc.php
+++ database/products.inc.php
@@ -23,13 +23,41 @@ function createProduct($name, $description, $price)
     $connection->close();
 }
 
-function readProduct ($id){
-
+function readProduct($id)
+{
     $connection = getConn();
     echo $connection ? "success <br>" : "failed <br>";
 
-    if (isset($id)){
-        
+    if (isset($id)) {
+        $stmt = $connection->prepare("SELECT * FROM products WHERE id = ?");
+        $stmt->bind_param("i", $id);
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["description"]. " " . $row["price"]. " " . $row["updated_at"]. " " . $row["created_at"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $stmt->close();
     }
 
+    $result = $connection->query("SELECT * FROM products");
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["description"]. " " . $row["price"]. " " . $row["updated_at"]. " " . $row["created_at"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $connection->close();
 }
--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 
 include './database/products.inc.php';
 
-createProduct ("hej", "hej", 22.50);
+// createProduct ("Sko", "super flotte Sko", 22.50);
 
+// createProduct ("Hat", "super flot Hat", 22.50);
 
+// createProduct ("Bukser", "super flotte Bukser", 22.50);
+
+// createProduct ("Skjorte", "super flot Skjorte", 22.50);
+
+readProduct(null);
 
 
 
